7ddab2196f6b9af1af22ff144c351a9fc18daa75
Reshmika <113816308+Reshmika19@users.noreply.github.com>
2025-10-23 15:00:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+ 
+app = FastAPI()
+ 
+# Allow frontend to call backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # we'll tighten this later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+ 
+@app.get("/")
+def root():
+    return {"message": "Hello from FastAPI"}
+ 
+@app.get("/api/hello")
+def hello(name: str = "Reshmika"):
+    return {"greeting": f"Hello, {name}! Welcome to FastAPI"}
